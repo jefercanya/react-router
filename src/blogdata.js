058e481d2e25ac3d1b4f8de62bd7fe07ae5f997d
jefercanya <jefercanya@hotmail.com>
2023-05-13 18:44:10 -0500

    Change blogdata.js

--- src/blogdata.js
+++ src/blogdata.js
@@ -3,19 +3,19 @@ blogdata.push({
   title: '¿Qué es React?',
   slug: 'que-es-react',
   content: 'React es el mejor framework de JavaScript.',
-  author: 'juandc',
+  author: 'Author 1',
 });
 blogdata.push({
   title: '¿Qué es Vue?',
   slug: 'que-es-vue',
   content: 'Vue es el mejor framework de JavaScript.',
-  author: 'diannerd',
+  author: 'Author 2',
 });
 blogdata.push({
   title: '¿Qué es Angular?',
   slug: 'que-es-angular',
   content: 'Angular es el mejor framework de JavaScript.',
-  author: 'nicobytes',
+  author: 'Author 3',
 });
 
 export { blogdata };
