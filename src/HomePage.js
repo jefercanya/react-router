8eeb8aef060b74fa0f5d0f6b9bb367e2f8d67750
jefercanya <jefercanya@hotmail.com>
2023-05-13 18:49:14 -0500

    Add Bootstrap and react-bootstrap

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+//import Container  from 'react-bootstrap/Container';
+
+// function HomePage() {
+//   return (
+//     <>
+//       <Container>
+//         <h1>HomePage</h1>
+//         <Button variant="primary">Hola mundo</Button>
+//       </Container>
+//     </>
+//   );
+// }
 
 function HomePage() {
   return (
-    <h1>HomePage</h1>
+    
+      <Container>
+        <h1>HomePage</h1>
+        <Button variant="primary">Hola mundo</Button>
+      </Container>
+    
   );
 }
 
--- src/Menu.js
+++ src/Menu.js
@@ -1,61 +1,91 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+// function Menu() {
+//   return (
+//     <nav>
+//       <ul>
+//         {routes.map(route => (
+//           <li>
+//             <NavLink
+//               style={({ isActive }) => ({
+//                 color: isActive ? 'red' : 'blue',
+//               })}
+//               to={route.to}
+//             >
+//               {route.text}
+//             </NavLink>
+//           </li>
+//         ))}
+        
+//         {/* <li>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link to="/blog">Blog</Link>
+//         </li>
+//         <li>
+//           <Link to="/profile">Profile</Link>
+//         </li> */}
+
+//         {/* <li>
+//           <NavLink
+//             style={({ isActive }) => ({
+//               color: isActive ? 'red' : 'blue',
+//             })}
+//             to="/"
+//           >Home</NavLink>
+//         </li>
+//         <li>
+//           <NavLink
+//             style={({ isActive }) => ({
+//               color: isActive ? 'red' : 'blue',
+//             })}
+//             to="/blog"
+//           >Blog</NavLink>
+//         </li>
+//         <li>
+//           <NavLink
+//             style={({ isActive }) => ({
+//               color: isActive ? 'red' : 'blue',
+//             })}
+//             to="/profile"
+//           >Profile</NavLink>
+//         </li> */}
+//       </ul>
+//     </nav>
+//   );
+// }
 
 function Menu() {
   return (
-    <nav>
-      <ul>
-        {routes.map(route => (
-          <li>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? 'red' : 'blue',
-              })}
-              to={route.to}
-            >
-              {route.text}
-            </NavLink>
-          </li>
-        ))}
-        
-        {/* <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li> */}
-
-        {/* <li>
-          <NavLink
-            style={({ isActive }) => ({
-              color: isActive ? 'red' : 'blue',
-            })}
-            to="/"
-          >Home</NavLink>
-        </li>
-        <li>
-          <NavLink
-            style={({ isActive }) => ({
-              color: isActive ? 'red' : 'blue',
-            })}
-            to="/blog"
-          >Blog</NavLink>
-        </li>
-        <li>
-          <NavLink
-            style={({ isActive }) => ({
-              color: isActive ? 'red' : 'blue',
-            })}
-            to="/profile"
-          >Profile</NavLink>
-        </li> */}
-      </ul>
-    </nav>
+    <Container>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">My App</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          {routes.map(route => (
+            <Nav.Item key={route.to}>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? 'red' : 'blue',
+                })}
+                className="nav-link"
+                to={route.to}
+              >
+                {route.text}
+              </NavLink>
+            </Nav.Item>
+          ))}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    </Container>
   );
 }
+
 
 const routes = [];
 routes.push({
